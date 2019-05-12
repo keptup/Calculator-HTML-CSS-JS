a4a4c768e0add48721162830771a9a6884c9eddb
Karolis <keptup@users.noreply.github.com>
2019-05-12 09:53:49 +0300

    Declare variables

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ const input = document.querySelector('.result');
 const zero = document.querySelector('.is-zero');
 const one = document.querySelector('.one');
 const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const four = document.querySelector('.four');
+const five = document.querySelector('.five');
