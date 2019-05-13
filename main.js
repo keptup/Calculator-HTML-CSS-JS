42c7b99975499bf8ef7795fadfbc8814ba17d264
Karolis <keptup@users.noreply.github.com>
2019-05-13 23:54:40 +0300

    Declare variable

--- main.js
+++ main.js
@@ -7,3 +7,4 @@ const four = document.querySelector('.four');
 const five = document.querySelector('.five');
 const six = document.querySelector('.six');
 const seven = document.querySelector('.seven');
+const eight = document.querySelector('.eight');
